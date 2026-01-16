89da531c480d36c2dcbd8e34ed0c8027fdc2d78f
Aaron Po <apo2@uwo.ca>
2026-01-15 21:48:20 -0500

    Restructure project

--- API/API.Core/Program.cs
+++ API/API.Core/Program.cs
@@ -1,7 +1,6 @@
 using BusinessLayer.Services;
 using DataAccessLayer.Repositories;
 using DataAccessLayer.Sql;
-using WebAPI.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,12 +15,9 @@ builder.Services.AddScoped<IUserAccountRepository, UserAccountRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapOpenApi();
 
 app.UseHttpsRedirection();
 app.MapControllers();
